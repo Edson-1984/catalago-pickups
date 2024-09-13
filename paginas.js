e3ced262991ff2ffa4f4a7dd7b5b7574d66fcf35
Edson <contato.nobrega84@gmail.com>
2024-09-13 20:00:48 -0300

    rotacionando o carro parte 1

--- paginas.js
+++ paginas.js
@@ -1,3 +1,26 @@
-const container = document.querySelector(".contaneiner");
+const giro = document.querySelector(".giro");
 const image = document.querySelector(".car-image");
 
+const cursor = {
+    isDragging: false,
+    initialPosition: 0,
+
+};
+
+giro.addEventListener("mousedown", (event) => {
+    cursor.isDragging = true;
+    cursor.initialPosition = event.clientX;
+    
+
+});
+
+giro.addEventListener("mouseup", (event) => {
+    cursor.isDragging = false;
+    
+});
+
+giro.addEventListener("mousemove" , ({ clientX }) =>{
+   if (!cursor.isDragging) return;
+
+   
+})
